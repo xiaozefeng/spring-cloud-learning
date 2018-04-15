ea38cb9c130f25f60a4eb913623e60f3dccc6bc2
肖泽锋 <qq523107430@163.com>
2018-04-15 18:00:01 +0800

    添加order服务

---
+++ order/src/main/java/com/imooc/order/OrderApplication.java
@@ -0,0 +1,15 @@
+package com.imooc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaozefeng
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
